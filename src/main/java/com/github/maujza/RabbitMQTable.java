79e7fc5a88ebc99d661b0d109de42bde180e3873
maujza <maurojzalazar@gmail.com>
2023-07-06 00:20:49 -0300

    setting capabilities

--- src/main/java/com/github/maujza/RabbitMQTable.java
+++ src/main/java/com/github/maujza/RabbitMQTable.java
@@ -1,5 +1,6 @@
 package com.github.maujza;
 
+import static java.util.Arrays.asList;
 import com.github.maujza.read.RabbitMQScanBuilder;
 import org.apache.spark.sql.connector.catalog.SupportsRead;
 import org.apache.spark.sql.connector.catalog.Table;
@@ -7,9 +8,15 @@ import org.apache.spark.sql.connector.catalog.TableCapability;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.sql.util.CaseInsensitiveStringMap;
 import org.apache.spark.sql.connector.read.ScanBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
 import java.util.Set;
 
 public class RabbitMQTable implements Table, SupportsRead {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQTable.class);
+    private static final Set<TableCapability> TABLE_CAPABILITY_SET = new HashSet<>(asList(TableCapability.CONTINUOUS_READ));
     @Override
     public String name() {
         return null;
@@ -22,7 +29,7 @@ public class RabbitMQTable implements Table, SupportsRead {
 
     @Override
     public Set<TableCapability> capabilities() {
-        return null;
+        return TABLE_CAPABILITY_SET;
     }
 
     @Override
