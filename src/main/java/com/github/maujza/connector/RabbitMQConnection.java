b15ae49a6ff53a6cc645d08d0e8396e521cb42ef
maujza <maurojzalazar@gmail.com>
2023-07-08 23:34:20 -0300

    returning configured channel

--- src/main/java/com/github/maujza/connector/RabbitMQConnection.java
+++ src/main/java/com/github/maujza/connector/RabbitMQConnection.java
@@ -10,16 +10,20 @@ import java.io.IOException;
 
 public class RabbitMQConnection {
     private final RabbitMQConnectionConfig connectionConfig;
-
+    private transient Connection connection;
     private final String queueName;
     private transient Channel channel;
     private transient RabbitMQConsumer consumer;
-
     protected transient boolean autoAck;
 
     public RabbitMQConnection(RabbitMQConnectionConfig connectionConfig, String queueName) {
+        this(connectionConfig, queueName, null);
+    }
+
+    public RabbitMQConnection(RabbitMQConnectionConfig connectionConfig, String queueName, Connection connection) {
         this.queueName = queueName;
         this.connectionConfig = connectionConfig;
+        this.connection = connection;
     }
 
     protected ConnectionFactory setupConnectionFactory() throws Exception {
@@ -43,7 +47,27 @@ public class RabbitMQConnection {
     }
 
     public Channel getConfiguredChannel() {
-        return null;
+        try {
+            connection = setupConnection();
+            channel = setupChannel(connection);
+            if (channel == null) {
+                throw new RuntimeException("None of RabbitMQ channels are available");
+            }
+            setupQueue();
+
+            consumer = new RabbitMQConsumer(channel);
+
+            channel.basicConsume(queueName, false, consumer);
+
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Cannot create RMQ connection with "
+                            + queueName
+                            + " at "
+                            + connectionConfig.getHost(),
+                    e);
+        }
+        return channel;
     };
 
 }
