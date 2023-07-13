120aacbefb4e5eb9f91070d66dfd743d02d107f1
maujza <maurojzalazar@gmail.com>
2023-07-13 01:46:49 -0300

    using atomic lon

--- src/main/java/com/github/maujza/read/RabbitMQMicroBatchStream.java
+++ src/main/java/com/github/maujza/read/RabbitMQMicroBatchStream.java
@@ -4,19 +4,27 @@ import org.apache.spark.sql.connector.read.InputPartition;
 import org.apache.spark.sql.connector.read.PartitionReaderFactory;
 import org.apache.spark.sql.connector.read.streaming.MicroBatchStream;
 import org.apache.spark.sql.connector.read.streaming.Offset;
+import org.apache.spark.sql.execution.streaming.LongOffset;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.sql.util.CaseInsensitiveStringMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 public class RabbitMQMicroBatchStream implements MicroBatchStream {
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQMicroBatchStream.class);
     private final StructType schema;
     private final CaseInsensitiveStringMap options;
 
+    private AtomicLong offset; // Initialize this in the constructor.
+
+
     public RabbitMQMicroBatchStream(StructType schema, CaseInsensitiveStringMap options) {
         this.schema = schema;
         this.options = options;
+        this.offset = new AtomicLong(0);
+
     }
 
     @Override
@@ -29,8 +37,7 @@ public class RabbitMQMicroBatchStream implements MicroBatchStream {
 
     @Override
     public Offset latestOffset() {
-        LOGGER.info("RabbitMQ has no concept of offsets, latest offset null");
-        return null;
+        return new LongOffset(offset.getAndAdd(1000));
     }
 
     @Override
@@ -42,7 +49,7 @@ public class RabbitMQMicroBatchStream implements MicroBatchStream {
     @Override
     public Offset initialOffset() {
         LOGGER.info("RabbitMQ has no concept of offsets, initial offset null");
-        return null;
+        return new LongOffset(offset.get());
     }
 
     @Override
