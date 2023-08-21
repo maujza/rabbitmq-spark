37b8a6faca4976febb4b5464f86d0dd38ffd32a4
maujza <maurojzalazar@gmail.com>
2023-08-21 01:34:45 -0300

    refactoring partition reader

--- src/main/java/com/github/maujza/read/RabbitMQMicroBatchPartitionReader.java
+++ src/main/java/com/github/maujza/read/RabbitMQMicroBatchPartitionReader.java
@@ -12,11 +12,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class RabbitMQMicroBatchPartitionReader implements PartitionReader<InternalRow> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQMicroBatchPartitionReader.class);
     private static final int MAX_MESSAGE_COUNT = 1000;
+    private static final String QUEUE_NAME_OPTION = "queue_name";
+    private static final String TIME_LIMIT_OPTION = "time_limit";
+    private static final String MAX_MESSAGES_PER_PARTITION_OPTION = "max_messages_per_partition";
 
     private final RabbitMQConnection connection;
     private final long timeLimit;
@@ -27,15 +31,14 @@ public class RabbitMQMicroBatchPartitionReader implements PartitionReader<Intern
     private InternalRow currentRecord;
     private final RabbitMQConsumer consumer;
     private final RabbitMQMessageToRowConverter rabbitMQMessageToRowConverter;
-    private String currentMessage;
 
     public RabbitMQMicroBatchPartitionReader(final RabbitMQMessageToRowConverter rabbitMQMessageToRowConverter, final RabbitMQConnectionConfig connectionConfig, final SerializableCaseInsensitiveStringMap options) {
         this.rabbitMQMessageToRowConverter = rabbitMQMessageToRowConverter;
-        this.connection = new RabbitMQConnection(connectionConfig, options.get("queue_name"));
+        this.connection = new RabbitMQConnection(connectionConfig, options.get(QUEUE_NAME_OPTION));
         this.consumer = connection.getConsumerFromConfiguredChannel();
-        this.timeLimit = Long.parseLong(options.get("time_limit"));
-        this.max_messages_per_partition = options.containsKey("max_messages_per_partition") ?
-                Long.parseLong(options.get("max_messages_per_partition")) : MAX_MESSAGE_COUNT;
+        this.timeLimit = Long.parseLong(options.get(TIME_LIMIT_OPTION));
+        this.max_messages_per_partition = options.containsKey(MAX_MESSAGES_PER_PARTITION_OPTION) ?
+                Long.parseLong(options.get(MAX_MESSAGES_PER_PARTITION_OPTION)) : MAX_MESSAGE_COUNT;
         this.startTime = System.currentTimeMillis();
     }
 
@@ -47,14 +50,13 @@ public class RabbitMQMicroBatchPartitionReader implements PartitionReader<Intern
     public boolean next() throws IOException {
         try {
             currentDelivery = consumer.nextDelivery();
-            currentMessage = new String(currentDelivery.getBody(), "UTF-8");
             if (shouldTerminate(currentDelivery.getEnvelope().getDeliveryTag())) {
                 return false;
             }
-            currentRecord = rabbitMQMessageToRowConverter.convertToInternalRow(currentMessage);
+            currentRecord = rabbitMQMessageToRowConverter.convertToInternalRow(new String(currentDelivery.getBody(), StandardCharsets.UTF_8));
             return true;
         } catch (Exception e) {
-            LOGGER.error("Error occurred while fetching next message", e);
+            LOGGER.error("Error occurred while fetching next message. Delivery: " + currentDelivery, e);
             return false;
         }
     }
@@ -65,7 +67,7 @@ public class RabbitMQMicroBatchPartitionReader implements PartitionReader<Intern
             LOGGER.info("Acknowledging and committing transaction");
             consumer.ack(currentDelivery.getEnvelope().getDeliveryTag());
         } catch (IOException e) {
-            LOGGER.error("Error occurred while acknowledging or committing transaction, attempting rollback", e);
+            LOGGER.error("Error occurred while acknowledging or committing transaction for delivery: " + currentDelivery, e);
         }
         return currentRecord;
     }
