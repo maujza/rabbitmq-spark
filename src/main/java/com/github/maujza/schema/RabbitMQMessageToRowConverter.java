cc69f71742c2c4ae65f6789a310f6907ca800556
maujza <maurojzalazar@gmail.com>
2023-10-01 18:39:01 -0300

    adding new way of handling messages

--- src/main/java/com/github/maujza/schema/RabbitMQMessageToRowConverter.java
+++ src/main/java/com/github/maujza/schema/RabbitMQMessageToRowConverter.java
@@ -1,93 +1,91 @@
 package com.github.maujza.schema;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.spark.sql.catalyst.InternalRow;
-import org.apache.spark.sql.catalyst.expressions.GenericInternalRow;
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema;
 import org.apache.spark.sql.types.*;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.apache.spark.unsafe.types.UTF8String;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.spark.sql.Row;
+import java.util.function.Function;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 public class RabbitMQMessageToRowConverter implements Serializable {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQMessageToRowConverter.class);
 
-
     private final StructType schema;
     private final StructField[] fields;
+    private final ObjectMapper objectMapper;
+    private final Function<Row, InternalRow> rowToInternalRowFunction;
 
     public RabbitMQMessageToRowConverter(StructType schema) {
         this.schema = schema;
         this.fields = schema.fields();
+        this.objectMapper = new ObjectMapper();
+        this.rowToInternalRowFunction = new RowToInternalRowFunction(schema);
         LOGGER.info("Initialized RabbitMQMessageToRowConverter with given schema");
-
     }
 
     public InternalRow convertToInternalRow(String message) {
-        LOGGER.debug("Converting message to InternalRow: {}", message);
-
-        return convertToInternalRow(new JSONObject(message));
+        try {
+            JsonNode rootNode = objectMapper.readTree(message);
+            return rowToInternalRowFunction.apply(convertToInternalRow(rootNode, this.schema));
+        } catch (IOException e) {
+            LOGGER.error("Failed to convert message to JsonNode: {}", e.getMessage());
+            return null;
+        }
     }
 
-    private InternalRow convertToInternalRow(JSONObject jsonObject) {
-        LOGGER.debug("Converting JSONObject to InternalRow: {}", jsonObject.toString());
-
+    private GenericRowWithSchema convertToInternalRow(JsonNode node, StructType schema) {
         Object[] values = new Object[fields.length];
+        populateValues(node, values);
+        return new GenericRowWithSchema(values, schema);
+    }
+
+    private void populateValues(JsonNode node, Object[] values) {
         for (int i = 0; i < fields.length; i++) {
             StructField field = fields[i];
-            values[i] = getValue(jsonObject, field.name(), field.dataType());
+            JsonNode fieldNode = node.get(field.name());
+            values[i] = getValue(fieldNode, field.dataType());
         }
-        return new GenericInternalRow(values);
     }
 
-    private Object getValue(JSONObject jsonObject, String name, DataType dataType) {
-        LOGGER.debug("Extracting value for field: {} with type: {}", name, dataType);
-
-        if (jsonObject.isNull(name)) {
+    private Object getValue(JsonNode node, DataType dataType) {
+        if (node == null || node.isNull()) {
             return null;
         }
 
-        if (dataType instanceof StringType) return UTF8String.fromString(jsonObject.getString(name));
-        if (dataType instanceof IntegerType) return jsonObject.getInt(name);
-        if (dataType instanceof LongType) return jsonObject.getLong(name);
-        if (dataType instanceof DoubleType) return jsonObject.getDouble(name);
-        if (dataType instanceof BooleanType) return jsonObject.getBoolean(name);
-        if (dataType instanceof StructType) return convertToInternalRow(jsonObject.getJSONObject(name));
-        if (dataType instanceof ArrayType) return extractList(jsonObject.getJSONArray(name), ((ArrayType) dataType).elementType());
-
-        LOGGER.warn("Unsupported data type: {}", dataType);
-        throw new UnsupportedOperationException("Unsupported data type: " + dataType);
+        switch (dataType.typeName()) {
+            case "string":
+                return UTF8String.fromString(node.asText());
+            case "integer":
+                return node.asInt();
+            case "long":
+                return node.asLong();
+            case "double":
+                return node.asDouble();
+            case "boolean":
+                return node.asBoolean();
+            case "struct":
+                return convertToInternalRow(node, (StructType) dataType);
+            case "array":
+                return extractList(node, ((ArrayType) dataType).elementType());
+            default:
+                LOGGER.warn("Unsupported data type: {}", dataType);
+                throw new UnsupportedOperationException("Unsupported data type: " + dataType);
+        }
     }
 
-    private Object[] extractList(JSONArray jsonArray, DataType elementType) {
-        LOGGER.debug("Extracting list with element type: {}", elementType);
-
-        Object[] array = new Object[jsonArray.length()];
-        for (int i = 0; i < jsonArray.length(); i++) {
-            if (elementType instanceof StructType) {
-                array[i] = convertToInternalRow(jsonArray.getJSONObject(i));
-            } else if (elementType instanceof ArrayType) {
-                array[i] = extractList(jsonArray.getJSONArray(i), ((ArrayType) elementType).elementType());
-            } else {
-                array[i] = getValue(jsonArray, i, elementType);
-            }
+    private Object[] extractList(JsonNode arrayNode, DataType elementType) {
+        Object[] array = new Object[arrayNode.size()];
+        for (int i = 0; i < arrayNode.size(); i++) {
+            array[i] = getValue(arrayNode.get(i), elementType);
         }
         return array;
-    }
-
-    private Object getValue(JSONArray jsonArray, int index, DataType dataType) {
-        LOGGER.debug("Extracting value from array at index: {} with type: {}", index, dataType);
-
-        if (dataType instanceof StringType) return UTF8String.fromString(jsonArray.getString(index));
-        if (dataType instanceof IntegerType) return jsonArray.getInt(index);
-        if (dataType instanceof LongType) return jsonArray.getLong(index);
-        if (dataType instanceof DoubleType) return jsonArray.getDouble(index);
-        if (dataType instanceof BooleanType) return jsonArray.getBoolean(index);
-
-        LOGGER.warn("Unsupported data type in array: {}", dataType);
-        throw new UnsupportedOperationException("Unsupported data type in array: " + dataType);
     }
 }
