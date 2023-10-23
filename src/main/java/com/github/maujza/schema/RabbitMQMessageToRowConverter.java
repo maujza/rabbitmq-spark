65252d8cb6752e61d4f3298b3d3d9d0041fa5fd0
maujza <maurojzalazar@gmail.com>
2023-10-23 07:06:44 -0300

    fixing issue with nested structs

--- src/main/java/com/github/maujza/schema/RabbitMQMessageToRowConverter.java
+++ src/main/java/com/github/maujza/schema/RabbitMQMessageToRowConverter.java
@@ -1,93 +1,96 @@
 package com.github.maujza.schema;
 
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.catalyst.expressions.GenericInternalRow;
 import org.apache.spark.sql.types.*;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.apache.spark.unsafe.types.UTF8String;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RabbitMQMessageToRowConverter implements Serializable {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQMessageToRowConverter.class);
-
-
     private final StructType schema;
-    private final StructField[] fields;
+    private final ObjectMapper objectMapper;
+    private final SimpleDateFormat timestampFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 
     public RabbitMQMessageToRowConverter(StructType schema) {
         this.schema = schema;
-        this.fields = schema.fields();
-        LOGGER.info("Initialized RabbitMQMessageToRowConverter with given schema");
-
+        this.objectMapper = new ObjectMapper();
     }
 
-    public InternalRow convertToInternalRow(String message) {
-        LOGGER.debug("Converting message to InternalRow: {}", message);
-
-        return convertToInternalRow(new JSONObject(message));
-    }
-
-    private InternalRow convertToInternalRow(JSONObject jsonObject) {
-        LOGGER.debug("Converting JSONObject to InternalRow: {}", jsonObject.toString());
-
-        Object[] values = new Object[fields.length];
-        for (int i = 0; i < fields.length; i++) {
-            StructField field = fields[i];
-            values[i] = getValue(jsonObject, field.name(), field.dataType());
-        }
-        return new GenericInternalRow(values);
-    }
-
-    private Object getValue(JSONObject jsonObject, String name, DataType dataType) {
-        LOGGER.debug("Extracting value for field: {} with type: {}", name, dataType);
-
-        if (jsonObject.isNull(name)) {
+    private Object convertField(Object jsonValue, DataType dataType) {
+        if (jsonValue == null) {
             return null;
         }
 
-        if (dataType instanceof StringType) return UTF8String.fromString(jsonObject.getString(name));
-        if (dataType instanceof IntegerType) return jsonObject.getInt(name);
-        if (dataType instanceof LongType) return jsonObject.getLong(name);
-        if (dataType instanceof DoubleType) return jsonObject.getDouble(name);
-        if (dataType instanceof BooleanType) return jsonObject.getBoolean(name);
-        if (dataType instanceof StructType) return convertToInternalRow(jsonObject.getJSONObject(name));
-        if (dataType instanceof ArrayType) return extractList(jsonObject.getJSONArray(name), ((ArrayType) dataType).elementType());
-
-        LOGGER.warn("Unsupported data type: {}", dataType);
-        throw new UnsupportedOperationException("Unsupported data type: " + dataType);
-    }
-
-    private Object[] extractList(JSONArray jsonArray, DataType elementType) {
-        LOGGER.debug("Extracting list with element type: {}", elementType);
-
-        Object[] array = new Object[jsonArray.length()];
-        for (int i = 0; i < jsonArray.length(); i++) {
-            if (elementType instanceof StructType) {
-                array[i] = convertToInternalRow(jsonArray.getJSONObject(i));
-            } else if (elementType instanceof ArrayType) {
-                array[i] = extractList(jsonArray.getJSONArray(i), ((ArrayType) elementType).elementType());
-            } else {
-                array[i] = getValue(jsonArray, i, elementType);
-            }
+        switch (dataType.typeName()) {
+            case "string":
+                return UTF8String.fromString(jsonValue.toString());
+            case "integer":
+                return Integer.parseInt(jsonValue.toString());
+            case "double":
+                return Double.parseDouble(jsonValue.toString());
+            case "boolean":
+                return Boolean.parseBoolean(jsonValue.toString());
+            case "timestamp":
+                try {
+                    return new Timestamp(timestampFormat.parse(jsonValue.toString()).getTime());
+                } catch (Exception e) {
+                    return null; // handle parse exceptions
+                }
+            case "array":
+                List<?> jsonList = (List<?>) jsonValue;
+                ArrayType arrayType = (ArrayType) dataType;
+                List<Object> convertedList = new ArrayList<>();
+                for (Object elem : jsonList) {
+                    convertedList.add(convertField(elem, arrayType.elementType()));
+                }
+                return convertedList.toArray();
+            case "map":
+                Map<?, ?> jsonMap = (Map<?, ?>) jsonValue;
+                MapType mapType = (MapType) dataType;
+                Map<Object, Object> convertedMap = new HashMap<>();
+                for (Map.Entry<?, ?> entry : jsonMap.entrySet()) {
+                    convertedMap.put(
+                            convertField(entry.getKey(), mapType.keyType()),
+                            convertField(entry.getValue(), mapType.valueType())
+                    );
+                }
+                return convertedMap;
+            case "struct":
+                if (jsonValue instanceof Map) {
+                    @SuppressWarnings("unchecked") // Safely cast after instanceof check
+                    Map<String, Object> nestedJsonMap = (Map<String, Object>) jsonValue;
+                    return convertToInternalRow(nestedJsonMap, (StructType) dataType);
+                }
+                return null;
+            default:
+                return null; // default to null for unsupported data types
         }
-        return array;
     }
 
-    private Object getValue(JSONArray jsonArray, int index, DataType dataType) {
-        LOGGER.debug("Extracting value from array at index: {} with type: {}", index, dataType);
+    public InternalRow convertToInternalRow(Map<String, Object> jsonMap, StructType schema) {
+        Object[] values = new Object[schema.size()];
 
-        if (dataType instanceof StringType) return UTF8String.fromString(jsonArray.getString(index));
-        if (dataType instanceof IntegerType) return jsonArray.getInt(index);
-        if (dataType instanceof LongType) return jsonArray.getLong(index);
-        if (dataType instanceof DoubleType) return jsonArray.getDouble(index);
-        if (dataType instanceof BooleanType) return jsonArray.getBoolean(index);
+        for (int i = 0; i < schema.size(); i++) {
+            StructField field = schema.fields()[i];
+            Object value = jsonMap.get(field.name());
+            values[i] = convertField(value, field.dataType());
+        }
 
-        LOGGER.warn("Unsupported data type in array: {}", dataType);
-        throw new UnsupportedOperationException("Unsupported data type in array: " + dataType);
+        return new GenericInternalRow(values);
+    }
+
+    public InternalRow convertToInternalRow(String message) throws Exception {
+        Map<String, Object> deserializedMessage = objectMapper.readValue(message, new TypeReference<Map<String, Object>>() {});
+        return convertToInternalRow(deserializedMessage, schema);
     }
 }
